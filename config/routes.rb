5d337b56dcf8613bbb0d5569c375b42f205b9271
gpanebra <gpanebramoina@gmail.com>
2020-05-18 19:51:08 -0500

    finish tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :articles
+  get 'welcome/index'
+  root 'welcome#index' #First route
+	get 'welcome', to: 'welcome#index' #Second route
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
